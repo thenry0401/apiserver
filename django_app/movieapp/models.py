7accfacbe0c3a0b87daf9af42907f6d0fa4be3af
thenry <thenry0401@gmail.com>
2018-09-04 21:38:57 +0900

    moive_info 모델 작성, 마이그레이션

--- django_app/movieapp/models.py
+++ django_app/movieapp/models.py
@@ -1,3 +1,42 @@
 from django.db import models
 
-# Create your models here.
+
+class MovieInfo(models.Model):
+    id = models.IntegerField(primary_key=True)
+    url = models.URLField()
+    imdb_code = models.CharField(max_length=30)
+    title = models.CharField(max_length=30)
+    title_english = models.CharField(max_length=30)
+    title_long = models.CharField(max_length=30)
+    slug = models.CharField(max_length=30)
+    year = models.IntegerField()
+    rating = models.IntegerField()
+    runtime = models.IntegerField()
+    genres = models.CharField(max_length=30)
+    summary = models.TextField()
+    decription_full = models.TextField()
+    synopsis = models.TextField()
+    yt_trailer_code = models.CharField(max_length=30)
+    language = models.CharField(max_length=30)
+    mpa_rating = models.CharField(max_length=30)
+    background_image = models.ImageField()
+    background_image_original = models.ImageField()
+    small_cover_image = models.ImageField()
+    medium_cover_image = models.ImageField()
+    large_cover_image = models.ImageField()
+    state = models.CharField(max_length=30)
+    torrents = models.ManyToManyField('Torrent')
+    date_uploaded = models.DateTimeField()
+    date_uploaded_unix = models.IntegerField()
+
+
+class Torrent(models.Model):
+    url = models.URLField()
+    hash = models.CharField(max_length=200)
+    quality = models.CharField(max_length=10)
+    seeds = models.IntegerField()
+    peers = models.IntegerField()
+    size = models.CharField(max_length=10)
+    size_bytes = models.IntegerField()
+    date_uploaded = models.DateTimeField
+    date_uploaded_unix = models.IntegerField()
