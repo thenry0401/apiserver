85a135732447aa64c01a7003ed5c532e7f755978
thenry <thenry0401@gmail.com>
2018-09-05 04:05:38 +0900

    api url 작성

--- django_app/config/urls.py
+++ django_app/config/urls.py
@@ -13,9 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path
 
+from movieapp import api
+
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    url(r'^list/$', api.MovieListView.as_view(), name='movie_list'),
+    url(r'^craete/$', api.MovieCreateView.as_view(), name='movie_create'),
 ]
